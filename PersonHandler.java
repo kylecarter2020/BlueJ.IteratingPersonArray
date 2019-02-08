738de83e529ac47f7e65fc90d1ef6d48fffd06ba
Kyle Carter <kylecarter@zipcoders-MacBook-Pro-9.local>
2019-02-08 11:36:30 -0500

    iterated through person using for, for each and while loop

--- PersonHandler.java
+++ PersonHandler.java
@@ -1,4 +1,4 @@
-package com.zipcodewilmington;
+ 
 
 /**
  * Created by leon on 1/24/18.
@@ -12,6 +12,15 @@ public class PersonHandler {
 
     public String whileLoop() {
         String result = "";
+        int counter = 0;
+        
+        while(counter < personArray.length)
+        {
+            result += personArray[counter];
+            counter++;
+        }
+        
+        
         // assume there is a `counter`
         // while `counter` is less than length of array
             // begin loop
@@ -28,6 +37,11 @@ public class PersonHandler {
 
     public String forLoop() {
         String result = "";
+        
+        for(int i = 0; i < personArray.length; i++)
+        {
+            result += personArray[i];
+        }
         // identify initial value
         // identify terminal condition
         // identify increment
@@ -46,6 +60,11 @@ public class PersonHandler {
 
     public String forEachLoop() {
         String result = "";
+        
+        for(Person p: personArray)
+        {
+            result += p;   
+        }
         // identify array's type
         // identify array's variable-name
 
